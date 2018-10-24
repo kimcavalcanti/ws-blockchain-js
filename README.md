a85a202eeed6b1ddb300a0439ccbd1d71e2b3bdd
Kim Cavalcanti <kimcavalcanti@MacBook-Pro-de-Kim.local>
2018-10-24 11:22:57 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Workshop Solutis
+
+> Simple blockchain structure developed for learning purposes.
+
+## Initialize
+
+```console
+npm install
+npm run dev
+```
+
+## License
+
+MIT
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { SHA256 } from 'crypto-js';
+
+class App {
+
+    constructor() {
+        console.info('App has been initialized');
+    }
+}
+
+const initialize = new App();
